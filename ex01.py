7b3157af9c19e2e7c3fc46b8df70893ccb4e7cdb
sutinan liamthong <zero19223@gmail.com>
2019-12-27 11:15:44 +0700

    17 commit

--- ex01.py
+++ ex01.py
@@ -1,6 +1,11 @@
-a = 8
-b = 4*2
-c = 16
-
-a = b 
-a != c
+score = 75
+if(score >= 80):
+    print("Grade A")
+elif(score >= 70):
+    print("Grade B")
+elif(score >= 60):
+    print("Grade C")
+elif(score >=50):
+    print("Grade D")
+else:
+    print("Grade F")
